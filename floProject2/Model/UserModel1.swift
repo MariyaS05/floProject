da2c799f7b77dd92dc33dad21b306435b7a29903
Mariya Govorushko <mariya_govorushko@mail.ru>
2023-06-02 17:38:09 +0300

    Gitignore added

---
+++ floProject2/Model/UserModel1.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel1.swift
+//  floProject2
+//
+//  Created by Мария  on 25.01.23.
+//
+
+import Foundation
---
+++ floProject2/Views/View/PersonalCardView.swift
@@ -0,0 +1,20 @@
+//
+//  PersonalCardView.swift
+//  floProject2
+//
+//  Created by Мария  on 25.02.23.
+//
+
+import UIKit
+
+class PersonalCardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
